89d1ae86d1743572fb915c5dd4e3e5d8b8e07c81
c <themonstermora@gmail.com>
2019-03-30 16:23:34 -0500

    this is a nigerian man

--- src/tours/Main.java
+++ src/tours/Main.java
@@ -64,7 +64,7 @@ public class Main {
 
 //Primero-ultimo
 	public Tour saveMethod1(Tour[] a) {
-		Double MaxSave=0.0;
+		Double MaxSave=Double.MAX_VALUE;
 		Nodo prim;
 		Nodo ult;
 		Nodo candidato1=null;;
@@ -78,28 +78,23 @@ public class Main {
 		}else {
 			for(int i=0; i<a.length;i++) {
 				prim=a[i].getT1()[0];
-				
 				for(int j=1; j<a.length;j++) {
 					ult=a[j].getT1()[a[j].getT1().length-1];
-					
-					
-					
+					System.out.println(prim.posX+","+prim.posY+"  "+ult.posX+","+ult.posY);
 					temp=distances(ult,prim);
 					if(MaxSave > temp) {
 						MaxSave=temp;
 						candidato1=prim;
 						candidato2=ult;
 						candi1Pos=i;
-						candi2Pos=j;
-						
+						candi2Pos=j;					
 					}	
 				}
 //				if(a[i+1].getT1().length==1) {
 //					ult=prim;
 //				}else {
 //					ult=a[i].getT1()[a[i].getT1().length-1];
-//				}
-					
+//				}			
 			}
 			System.out.println("("+candidato1.getPosX()+","+candidato1.getPosY()+") ("+candidato2.getPosX()+","+candidato2.getPosY()+") : "+ MaxSave+ "   "+ candi1Pos +"," +candi2Pos );
 		}
@@ -153,211 +148,4 @@ public class Main {
 		m.saveMethod1(t);
 	}
 	
-	public void o() {
-//	
-//		private Node[] cheapestTour;
-//	
-//		class Node {
-//			int x;
-//			int y;
-//			Node connectForward;
-//			Node connectBack;
-//			Node first;
-//			
-//			public Node(int x, int y) {
-//				this.x=x;
-//				this.y=y;
-//			}
-//			
-//			public void setX(int x) {
-//				this.x=x;
-//			}
-//			
-//			public void setY(int y) {
-//				this.y=y;
-//			}
-//			
-//			public int getX() {
-//				return x;
-//			}
-//			
-//			public int getY() {
-//				return y;
-//			}
-//			
-//			public Node getConnectF() {
-//				return connectForward;
-//			}
-//			
-//			public void setConnectF(Node connectForward) {
-//				this.connectForward=connectForward;
-//			}
-//			
-//			public Node getConnectB() {
-//				return connectBack;
-//			}
-//			
-//			public void setConnectB(Node connectBack) {
-//				this.connectForward=connectBack;
-//			}
-//		}
-//	
-//		public class Tour{
-//			Node[] t1;
-//			
-//			Tour(int size){
-//				t1=new Node[size];
-//			}
-//
-//			public Node[] getT1() {
-//				return t1;
-//			}
-//			
-//			public void setT1(Node[] t1) {
-//				this.t1=t1;
-//			}
-//			
-//		}
-//	
-//		private Node[] nodes;
-//		private Tour[] tours;
-//	
-//		private double[][] saving;
-//	
-//		public Main() {
-//			setMat(new Node[5]);
-//			setWarded(new Tour[5]);
-//			setSaving(new double[5][5]);
-//			initialize();
-//		}
-//	
-//		/**
-//		 * @return the mat
-//		 */
-//		public Node[] getMat() {
-//			return nodes;
-//		}
-//
-//		/**
-//		 * @param mat the mat to set
-//		 */
-//		public void setMat(Node[] nodes) {
-//			this.nodes = nodes;
-//		}
-//
-//		/**
-//		 * @return the saving
-//		 */
-//		public double[][] getSaving() {
-//			return saving;
-//		}
-//
-//		/**
-//		 * @param saving the saving to set
-//		 */
-//		public void setSaving(double[][] saving) {
-//			this.saving = saving;
-//		}
-//
-//		/**
-//		 * @return the cheapestTour
-//		 */
-//		public Node[] getCheapestTour() {
-//			return cheapestTour;
-//		}
-//
-//		/**
-//		 * @param cheapestTour the cheapestTour to set
-//		 */
-//		public void setCheapestTour(Node[] cheapestTour) {
-//			this.cheapestTour = cheapestTour;
-//		}
-//	
-//		/**
-//		 * @return the warded
-//		 */
-//		public Tour[] getWarded() {
-//			return tours;
-//		}
-//
-//		/**
-//		 * @param warded the warded to set
-//		 */
-//		public void setWarded(Tour[] tours) {
-//			this.tours=tours;
-//		}
-//
-//		public void initialize() {
-//			for(int i=0;i<saving.length;i++) {
-//				for(int j=0;j<saving[i].length;j++) {
-//					saving[i][j]=-99999;
-//				}
-//			}
-//			for(int i=0;i<saving.length;i++) {
-//				int x=(int) (Math.random()*20)+1;
-//				int y=(int) (Math.random()*20)+1;
-//				nodes[i]=new Node(x,y);
-//			}
-//			calculateSave();
-//		}
-//	
-//		public void joinTours1() {
-//			for(int i=1;i<nodes.length;i++) {
-//				nodes[0].setConnectF(nodes[i]);
-//				nodes[i].setConnectB(nodes[0]);
-//			}
-//		}
-//	
-//		public void calculateSave() {
-//			for(int i=0;i<nodes.length;i++) {
-//				for(int j=0;j<nodes.length;j++) {
-//					if(i!=j) {
-//						double save=distances(nodes[j].getX(), nodes[j].getY(), nodes[i].getX(), nodes[i].getY());
-//						saving[i][j]=save;
-//					}
-//				}
-//			}
-//		}
-//	
-//		public double distances(int x1, int y1, int x2, int y2) {
-//			double dX=x2-x1;
-//			double dY=y2-y1;
-//			return Math.sqrt( (Math.pow(dX,2)) + (Math.pow(dY,2)) );
-//		}
-//	
-//		public double save(Node a, Node b, Node c, Node d) {
-//			double s1=distances(a.getX(), a.getY(), c.getX(), c.getY());
-//			double s2=distances(a.getX(), a.getY(), d.getX(), d.getY());
-//			double s3=distances(b.getX(), b.getY(), c.getX(), c.getY());
-//			double s4=distances(a.getX(), a.getY(), d.getX(), d.getY());
-//			double[] menor= {s1, s2, s3, s4};
-//			int iMenor = 0;
-//		    for(int i=1;i<menor.length;i++){
-//		    	if(menor[i]<menor[iMenor]) {
-//		    		iMenor = i;
-//		    	}
-//		    }
-//		    return menor[iMenor];
-//		}
-//	
-//		public static void main(String[] args) {
-//			Main m=new Main();
-//			String eje="";
-//			String arr="";
-//			
-//			for (int i=0;i<m.getMat().length;i++) {
-//				arr+="("+m.getMat()[i].getX()+","+m.getMat()[i].getY()+")";
-//			}
-//			
-//			for(int i=0;i<m.getSaving().length;i++) {
-//				for(int j=0;j<m.getSaving()[i].length;j++) {
-//					eje+=m.getSaving()[i][j]+"\t";
-//				}
-//				eje+="\n";
-//			}
-//			System.out.println(eje);
-//			System.out.println(arr);
-//		}
-//
-	}
 }
